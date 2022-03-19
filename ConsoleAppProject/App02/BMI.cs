381a4de59fbd79fad0dd3f68b160b1b28d132a44
EthanDR52 <91742651+EthanDR25@users.noreply.github.com>
2022-03-19 11:16:35 +0000

    Added BMI

--- ConsoleAppProject/App02/BMI.cs
+++ ConsoleAppProject/App02/BMI.cs
@@ -1,4 +1,8 @@
-﻿namespace ConsoleAppProject.App02
+﻿using ConsoleAppProject;
+using ConsoleAppProject.App02;
+using System;
+
+namespace ConsoleAppProject.App02
 {
     /// <summary>
     /// Please describe the main features of this App
@@ -6,7 +10,51 @@
     /// <author>
     /// Student Name version 0.1
     /// </author>
+    /// 
     public class BMI
     {
+        public double weight;
+        public double height;
+
+        public void Run()
+        {
+            OutputHeading();
+            CalcuateBMI();
+        }
+        public void OutputHeading()
+        {
+            Console.WriteLine("App 2 - BMI Calculator");
+            Console.WriteLine("         by           ");
+            Console.WriteLine("  Ethan Dias-Richards ");
+        }
+
+        public void CalcuateBMI()
+        {
+            Console.WriteLine("Please enter your weight: ");
+            string weightvalue = Console.ReadLine();
+            weight = Convert.ToDouble(weightvalue);
+
+            Console.WriteLine("Please enter your height: ");
+            string heightvalue = Console.ReadLine();
+            height = Convert.ToDouble(heightvalue);
+
+            double bmi = weight / height;
+
+            Console.WriteLine("Your BMI is " + bmi);
+
+        }
+        public static void Main(String[] args)
+        {
+            BMI bmi = new BMI();
+
+            bmi.OutputHeading();
+            bmi.CalcuateBMI();
+
+
+        }
     }
-}
+
+   }
+
+
+
